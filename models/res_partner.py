3c03ca7759991254bd3aeed045375dd9c83f2fbe
puzplaiaundi <puzquiano@plaiaundi.com>
2026-06-07 23:30:23 +0200

    Cambio en los nombres de los campos de nombre y apellidos de erasmus. Mostrar contactos erasmus en el modulo

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,9 +5,9 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-
-    primer_apellido = fields.Char(string='Primer apellido')
-    segundo_apellido = fields.Char(string='Segundo apellido')
+    erasmus_nombre = fields.Char(string='Nombre')
+    erasmus_apellido1 = fields.Char(string='Primer apellido')
+    erasmus_apellido2 = fields.Char(string='Segundo apellido')
     es_erasmus = fields.Boolean(string='Es Erasmus')
     tipo_contacto_erasmus_id = fields.Many2one(
         comodel_name='erasmus.tipo.contacto',
